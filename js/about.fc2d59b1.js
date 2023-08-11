fa7b43541d22d5a9dc7eb65e60d43f34d1e3aa7e
kafpliz <kafpliz@gmail.com>
2023-08-12 02:24:42 +0300

    gh-page

---
+++ js/about.fc2d59b1.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfindme_1"]=self["webpackChunkfindme_1"]||[]).push([[443],{833:function(n,e,u){u.r(e),u.d(e,{default:function(){return d}});var t=u(252);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function i(n,e){return(0,t.wg)(),(0,t.iD)("div",a,s)}var c=u(744);const f={},o=(0,c.Z)(f,[["render",i]]);var d=o}}]);
+//# sourceMappingURL=about.fc2d59b1.js.map
